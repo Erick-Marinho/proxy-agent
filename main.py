62543fe7f4725bb1fcc33fdfaf961eedfaf33192
Erick-Marinho <erickgaves@gmail.com>
2025-08-22 02:16:17 -0300

    Inserindo version em health

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ async def health_check():
     """
     Endpoint de health check
     """
-    return {"status": "healthy", "service": "proxy-agent-supervisor"}
+    return {"status": "healthy", "service": "proxy-agent-supervisor", "version": "1.1.0"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
